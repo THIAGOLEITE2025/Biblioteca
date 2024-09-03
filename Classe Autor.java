0a4bc91475e01e3c681a30bf70b5ab9b89fff2bd
THIAGOLEITE2025 <162601242+THIAGOLEITE2025@users.noreply.github.com>
2024-09-03 20:38:28 -0300

    Classe Autor.java

---
+++ Classe Autor.java
@@ -0,0 +1,14 @@
+public class Autor {
+    private final String nome;
+    private final String sobrenome;
+
+    public Autor(String nome, String sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " " + sobrenome;
+    }
+}
